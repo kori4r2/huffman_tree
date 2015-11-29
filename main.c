54f879d829994b14b7775e6e5f02e2e25b49f9e8
Kori4r2 <xitsilvax@hotmail.com>
2015-11-29 18:40:57 -0200

    faltava a main --'

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// Ítalo Tobler Silva - nUSP 8551910
 #include <stdio.h>
 #include <stdlib.h>
 #include "huffman_tree.h"
@@ -5,13 +6,21 @@
 
 int main(int argc, char *argv[]){
 
+	// Obtém uma string da stdin
 	char *input = my_get_line(stdin);
+	// Cria a árvore de huffman correspondente à string lida
 	TREE *tree = get_huffman_tree(input);
+	// Atualiza os códigos da árvore
 	update_codes(tree);
+	// coded recebe a string codificada
 	char *coded = code(input, tree);
+	// decoded recebe coded decodificada
 	char *decoded = decode(coded, tree);
+	// Imprime na stdout os itens armazenados na árvore
 	print_tree(tree);
+	// Imprime na stdout as strings obtidas a partir da codificação e decodificação
 	printf("\nencode: %s\n\ndecode: %s\n", coded, decoded);
+	// Libera a memória alocada
 	free(input);
 	free(coded);
 	free(decoded);
